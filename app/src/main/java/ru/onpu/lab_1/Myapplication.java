905ba1b8acee2ed0ecef852cbc4fbaf50668d43b
Yeolow <torettoskyline13@gmail.com>
2018-10-11 19:34:22 +0400

    Initial commit

---
+++ app/src/main/java/ru/onpu/lab_1/Myapplication.java
@@ -0,0 +1,13 @@
+package ru.onpu.lab_1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Myapplication extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_myapplication);
+    }
+}
